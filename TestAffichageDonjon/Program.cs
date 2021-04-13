7e3681b36496703a6fdde1fec0f3aaa610044342
Amerix <emma.marqueton@gmail.com>
2021-04-13 09:18:40 +0200

    Génération des pièces de donjon

---
+++ TestAffichageDonjon/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TestAffichageDonjon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int longueur = 20;
+            int largeur = 10;
+            string affichage = "╔";
+            for (int k = 0; k < longueur; k++)
+            {
+                affichage += "═";
+            }
+            affichage += "╗\n";
+
+
+            for (int i=0; i< largeur; i++)
+            {
+                affichage += "║";
+                for (int j=0; j< longueur; j++)
+                {
+                    affichage += ".";
+                }
+                affichage += "║\n";
+            }
+
+            affichage += "╚";
+            for (int k = 0; k < longueur; k++)
+            {
+                affichage += "═";
+            }
+            affichage += "╝\n";
+
+            Console.WriteLine(affichage);
+        }
+    }
+}
